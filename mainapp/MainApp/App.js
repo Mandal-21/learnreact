6295b1e8d75e4a45ce6a625cf8cd6b369fb1ec75
Amar <a.mandal@intelligencenode.com>
2022-02-06 11:57:17 +0530

    updated

--- mainapp/MainApp/App.js
+++ mainapp/MainApp/App.js
@@ -11,48 +11,40 @@ import {
   Linking,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
 
 const App = () => {
 
+  const [items, setItems] = useState([
+    { 'key': 1, 'item': 1 },
+    { 'key': 2, 'item': 2 },
+    { 'key': 3, 'item': 3 },
+    { 'key': 4, 'item': 4 },
+    { 'key': 5, 'item': 5 },
+    { 'key': 6, 'item': 6 },
+    { 'key': 7, 'item': 7 },
+    { 'key': 8, 'item': 8 },
+    { 'key': 12, 'item': 95 },
+    { 'key': 61, 'item': 10 },
+    { 'key': 33, 'item': 67 },
+    { 'key': 13, 'item': 0 },
+  ]);
+
   return (
     <View style={styles.body}>
-      <View style={styles.row}>
-        <View style={styles.view1}>
-          <Text style={styles.text}>1</Text>
-        </View>
-        <View style={styles.view2}>
-          <Text style={styles.text}>2</Text>
-        </View>
-        <View style={styles.view3}>
-          <Text style={styles.text}>3</Text>
-        </View>
-      </View>
-      <View style={styles.row}>
-        <View style={styles.view4}>
-          <Text style={styles.text}>4</Text>
-        </View>
-      </View>
-      <View style={styles.row}>
-        <View style={styles.view5}>
-          <Text style={styles.text}>5</Text>
-        </View>
-      </View>
-      <View style={styles.big_row}>
-        <View style={styles.view6}>
-          <Text style={styles.text}>6</Text>
-        </View>
-        <View style={styles.view7}>
-          <Text style={styles.text}>7</Text>
-        </View>
-      </View>
+
+      <ScrollView>
+        {
+          items.map((i) => {
+            return (
+              <View style={styles.item} key={i.key}>
+                <Text style={styles.text}>Item {i.item}</Text>
+              </View>
+            )
+          })
+        }
+      </ScrollView>
+
     </View>
   );
 };
@@ -62,64 +54,15 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
   },
-  row: {
-    flexDirection: 'row',
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  big_row: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: '#ffffff',
-    alignItems: 'stretch',
-    justifyContent: 'center',
-  },
-  view1: {
-    flex: 1,
-    backgroundColor: '#00ffff',
+
+  item: {
+    margin: 10,
+    backgroundColor: '#00f0f0',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  view2: {
-    flex: 2,
-    backgroundColor: '#ff00ff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view3: {
-    flex: 3,
-    backgroundColor: '#ffff00',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view4: {
-    flex: 1,
-    backgroundColor: '#ff0000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view5: {
-    flex: 1,
-    backgroundColor: '#0fff0f',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view6: {
-    flex: 1,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view7: {
-    flex: 1,
-    backgroundColor: '#0000ff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
   text: {
     color: '#000000',
     fontSize: 35,
